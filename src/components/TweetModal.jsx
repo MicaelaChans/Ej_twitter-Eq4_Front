3a18157fb2dc2ddb0c9b32b841488f31acb9cd84
Micaela Chans <micaelachansmoure@gmail.com>
2023-09-05 17:28:18 -0300

    Following

--- src/components/TweetModal.jsx
+++ src/components/TweetModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import NewTweet from "./NewTweet";
+import { useEffect } from "react";
 
 function TweetModal() {
   const [show, setShow] = useState(false);
