3b68e06b4c0243bef18a3a961b6405da530a2866
Leandro Matosas <lnmatosas@hotmail.com>
2023-09-06 10:29:36 -0300

    Orden de Tweets

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -31,8 +31,6 @@ function Profile() {
     getUsers();
   }, [username]);
 
-  console.log(paramsUser);
-
   return (
     paramsUser && (
       <div className="container">
@@ -45,62 +43,65 @@ function Profile() {
             <div className="border-bottom">
               <h5 className="m-0 mt-2 pb-2 borderTweet">Tweets</h5>
             </div>
-            {paramsUser.tweetsList.map((tweet) => (
-              <div key={tweet._id} className="form-floating mt-2">
-                <div className="row border-bottom my-2 py-3">
-                  <div className="col-1">
-                    <img
-                      src={paramsUser.profilePic}
-                      alt=""
-                      className="home-img"
-                    />
-                  </div>
-                  <div className="col-11">
-                    <div id="dat1">
-                      <a href="/" className="link-dark">
-                        <h5 className="m-0 d-inline-block">
-                          {paramsUser.firstname} {paramsUser.lastname}
-                        </h5>
-                      </a>
-                      <p className="m-0 d-inline-block">
-                        @{paramsUser.username} ·{" "}
-                        {tweet.createdAt.toLocaleDateString}
-                      </p>
+            {paramsUser.tweetsList
+              .map((tweet) => (
+                <div key={tweet._id} className="form-floating mt-2">
+                  <div className="row border-bottom my-2 py-3">
+                    <div className="col-1">
+                      <img
+                        src={paramsUser.profilePic}
+                        alt=""
+                        className="home-img"
+                      />
                     </div>
-                    <div>
-                      <p className="m-0">{tweet.content}</p>
-                    </div>
-                    <div className="container-flex">
-                      <div className="d-flex align-items-center">
-                        <div className="icono d-flex align-items-center">
-                          <a href="/tweet/like/" className="p-0 me-2 heart">
-                            <i className="bi bi-heart-fill text-danger"></i>
-                            <i className="bi bi-heart-fill text-dark"></i>
-                          </a>
-                          <p className="m-0">{tweet.likes.length}</p>
-                        </div>
-                        <div className="icono">
-                          <i className="bi bi-repeat"></i>
-                        </div>
-                        <div className="icono">
-                          <i className="bi bi-chat"></i>
-                        </div>
+                    <div className="col-11">
+                      <div id="dat1">
+                        <a href="/" className="link-dark">
+                          <h5 className="m-0 d-inline-block">
+                            {paramsUser.firstname} {paramsUser.lastname}
+                          </h5>
+                        </a>
+                        <p className="m-0 d-inline-block">
+                          @{paramsUser.username} ·{" "}
+                          {tweet.createdAt.toLocaleDateString}
+                        </p>
                       </div>
-                      <div className="d-flex">
-                        <form
-                          action="/tweet/tweet.id?_method=DELETE"
-                          method="get"
-                        >
-                          <button href="tweet.id">
-                            <i className="bi bi-trash-fill color: tomato"></i>
-                          </button>
-                        </form>
+                      <div>
+                        <p className="m-0">{tweet.content}</p>
+                      </div>
+                      <div className="container-flex">
+                        <div className="d-flex align-items-center">
+                          <div className="icono d-flex align-items-center">
+                            <a href="/tweet/like/" className="p-0 me-2 heart">
+                              <i className="bi bi-heart-fill text-danger"></i>
+                              <i className="bi bi-heart-fill text-dark"></i>
+                            </a>
+                            <p className="m-0">{tweet.likes.length}</p>
+                          </div>
+                          <div className="icono">
+                            <i className="bi bi-repeat"></i>
+                          </div>
+                          <div className="icono">
+                            <i className="bi bi-chat"></i>
+                          </div>
+                        </div>
+                        <div className="d-flex">
+                          <form
+                            action="/tweet/tweet.id?_method=DELETE"
+                            method="get"
+                          >
+                            <button href="tweet.id">
+                              <i className="bi bi-trash-fill color: tomato"></i>
+                            </button>
+                          </form>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+              .sort()
+              .reverse()}
           </div>
           <div className="col-xxl-3 col-xl-3 col-lg-4 d-none d-xl-block d-lg-block">
             <Sidebar />
